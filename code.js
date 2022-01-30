a71672f63f1e3871f76f4270cd9d32391ea3fdd0
Siddharth Gopakumar <siddharthgopi@outlook.com>
2022-01-30 10:00:07 +0530

    The search working now.

--- code.js
+++ code.js
@@ -1,2 +1,22 @@
-const url = 'https://api.lyrics.ovh/suggest/rihanna';
-fetch(url).then((data)=>data.json()).then(data => console.log(data["data"][0]["artist"]["name"]));
+const submitBtn = document.querySelector("button");
+
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const searchVal = document.querySelector("input").value;
+    const url = `https://api.lyrics.ovh/suggest/${searchVal}`;
+    fetch(url)
+    .then((data)=>data.json())
+    .then(data => {
+        for(let element of data["data"])   {    
+            console.log(element["title"] + " " + element["artist"]["name"]);
+            const div = document.createElement("div");
+            const para = document.createElement("para");
+            const button = document.createElement("button");
+            para.textContent = `${element["artist"]["name"]} ${element["title"]}`;
+            button.textContent= "Get Lyrics";
+            div.appendChild(para);
+            div.appendChild(button);
+            document.querySelector("#result").appendChild(div);
+        }
+    }).catch((er)=> console.log(er));
+});
